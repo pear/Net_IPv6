e606d3d8c92e75ac14a5f74671d10b98121eeee4
Alexander Merz <alexmerz@php.net>
2010-11-18 02:58:05 +0000

    Tagging the 1.2.0b release
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_IPv6/tags/Net_IPv6-1.2.0b@305480 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,32 @@
+<?php
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Net_IPv6_AllTests::main');
+}
+
+// PHPUnit inlcudes
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+
+require_once 'NetIPv6Test.php';
+
+class Net_IPv6_AllTests
+{
+    public static function main()
+    {
+        PHPUnit_TextUI_TestRunner::run(self::suite());
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('Net_IPv6 Tests');
+        $suite->addTestSuite('NetIPv6Test');
+        return $suite;
+    }
+}
+
+
+// exec test suite
+if (PHPUnit_MAIN_METHOD == 'Net_IPv6_AllTests::main') {
+    Net_IPv6_AllTests::main();
+}
+?>
