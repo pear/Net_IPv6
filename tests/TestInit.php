a309620f8d8a17ced05064063e9f84248d697e85
Daniel O'Connor <daniel.oconnor@gmail.com>
2014-03-22 18:33:20 +1030

    Fail more obviously

--- tests/TestInit.php
+++ tests/TestInit.php
@@ -1,7 +1,4 @@
 <?php
 $root = dirname(__DIR__);
 $autoloader = $root . '/vendor/autoload.php';
-if (!file_exists($autoloader)) {
-    die("Unable to find autoload." . PHP_EOL);
-}
 require $autoloader;
